9194d54bb102792d414d574a9adff1d890176e4c
Admin <k2annex@Admins-MacBook-Air.local>
2019-12-27 15:17:42 +0530

    apple pass

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+           <p>
+          <a href='https://q.passkit.net/~/#/p/h58UhfADSRqhDc' download>Click to download</a>
+           </p>
         <a
           className="App-link"
           href="https://reactjs.org"
